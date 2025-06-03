Fixed the radial boundary computation for the Fourier transforms. Made the chirp condition similar to the focusing one for symmetry.

codes/python/atfurpy/core/initial.py
@@ -24,14 +24,17 @@ def initialize_envelope(grid, laser):
     space_decaying_term = -(
         (grid.r_grid_2d / laser.params.waist) ** laser.gaussian_order
     )
-    time_decaying_term = (
-        -(1 + 1j * laser.params.chirp) * (grid.t_grid_2d / laser.params.duration) ** 2
-    )
+    time_decaying_term = -((grid.t_grid_2d / laser.params.duration) ** 2)
 
-    if laser.params.focal_length != 0:  # phase curvature due to focusing lens
+    if laser.params.focal_length != 0:  # phase curvature due to the focusing lens
         space_decaying_term = space_decaying_term + 0j
         space_decaying_term -= (
             0.5j * laser.wavenumber * grid.r_grid_2d**2 / laser.params.focal_length
+        )
+    if laser.params.chirp != 0:  # temporal phase due to the chirping system
+        time_decaying_term = time_decaying_term + 0j
+        time_decaying_term -= (
+            1j * laser.params.chirp * (grid.t_grid_2d / laser.params.duration) ** 2
         )
 
     return laser.amplitude * np.exp(space_decaying_term + time_decaying_term)

codes/python/atfurpy/results/routines.py
@@ -75,15 +75,15 @@ def cheap_diagnostics(solver, step):
     max_intensity_idx = np.argmax(np.abs(envelope_rt), axis=1)
     max_density_idx = np.argmax(density_rt, axis=1)
 
-    solver.envelope_r0_zt[step] = envelope_rt[0]
+    solver.envelope_r0_zt[step, :] = envelope_rt[0, :]
     solver.envelope_tp_rz[:, step] = envelope_rt[
         np.arange(envelope_rt.shape[0]), max_intensity_idx
     ]
-    solver.density_r0_zt[step] = density_rt[0]
+    solver.density_r0_zt[step, :] = density_rt[0, :]
     solver.density_tp_rz[:, step] = density_rt[
         np.arange(density_rt.shape[0]), max_density_idx
     ]
-    solver.fluence_rz[:, step] = fluence_r
+    solver.fluence_rz[:, step] = fluence_r[:]
     solver.radius_z[step] = radius[0]
 
 