dde0bf338811f4c7250ea8ee094dc1704e861d75
Ismael <126611721+glabu@users.noreply.github.com>
2025-07-15 20:11:56 +0200

    Added new content in docs/ file for documentation wiki using Sphinx.

---
+++ docs/source/conf.py
@@ -0,0 +1,60 @@
+"""Sphinx configuration file."""
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "acherus"
+copyright = "2025, Ismael Torres García"
+author = "Ismael Torres García"
+release = "0.7.0"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.doctest",
+    "sphinx.ext.extlinks",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.mathjax",  # for eqs
+    "sphinx.ext.napoleon",
+    "sphinx.ext.todo",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.githubpages",
+    "sphinx_copybutton",
+    "myst_parser",  # for markdown
+    "sphinx_design",
+    "sphinx_last_updated_by_git",
+]
+
+templates_path = ["_templates"]
+source_suffix = {
+    ".rst": "restructuredtext",
+    ".md": "markdown",
+}
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+# toctree master document.
+master_doc = "index"
+
+# Pygments (Python-driven syntax highlighting) style.
+pygments_style = "sphinx"
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["images"]
+# html_logo = "images/acherus-logo-pinkwhite.png"
+html_theme_options = {
+    "logo_only": True,
+    "home_page_in_toc": True,
+    "display_version": False,
+    "collapse_navigation": False,
+    "navigation_depth": 1,
+}
---
+++ docs/source/index.md
@@ -0,0 +1,6 @@
+.. toctree::
+   :caption: Contents:
+   :maxdepth: 2
+
+   index.md
+
