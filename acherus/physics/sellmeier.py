Replaced Peck's two-term air dispersion formula by the three-term one. Fixed a mistake in the variable used by Peck too.

acherus/physics/sellmeier.py
@@ -1,9 +1,9 @@
 """
 Semi-empirical full dispersion equation module.
 
-Air dispersion uses a two-term dispersion formula, with only four parameters,
+Air dispersion uses a three-term dispersion formula, with five parameters,
 from E. R. Peck and K. Reeder (1972). The wavelength validity range goes from
-0.23 microns (far-UV) to 1.69 microns (near-IR) at 15 degrees Celsius.
+0.185 microns (far-UV) to 1.69 microns (near-IR) at 15 degrees Celsius.
 
 Water dispersion uses a four-term dispersion formula, with five parameters,
 from K. D. Mielenz (1978). The wavelength validity range goes from
@@ -18,9 +18,8 @@ How this works
 Both functions have the angular frequency (rad/s) as argument and return the
 refraction index (non-dimensional) at that frequency.
 
-Peck's 'x' variable is the reciprocal of the vacuum wavelength squared, with
-the wavelength in units of microns. Thus, conversion from meters to microns
-is needed.
+Peck's 'x' variable is the vacuum wavenumber squared, in units of reciprocal
+microns. Thus, conversion from meters to microns is needed.
 
 Mielenz's 'x' variable is the vacuum wavelength in units of microns.
 Hence, the same unit conversion is computed. The same goes for Malitson's
@@ -33,12 +32,12 @@ from scipy.constants import c
 
 
 def sellmeier_air(omega):
-    wavelen = 2 * np.pi * c / omega
-    x = 1 / (wavelen * 1e6) ** 2
+    x = 1e-6 * omega / c
+    x2 = x ** 2
 
-    two_term = 5791817 / (238.0185 - x) + 167909 / (57.362 - x)
+    three_term = 8060.51 + 2480990 / (132.274 - x2) + 17455.7 / (39.32957 - x2)
 
-    return 1 + 1e-8 * two_term
+    return 1 + 1e-8 * three_term
 
 def sellmeier_water(omega):
     x = 1e6 * 2 * np.pi * c / omega
@@ -52,8 +51,8 @@ def sellmeier_silica(omega):
     x = 1e6 * 2 * np.pi * c / omega
     x2 = x ** 2
 
-    six_term = x2 * (
+    three_term = x2 * (
         0.6961663 / (x2 - 0.0684043**2) + 0.4079426 / (x2 - 0.1162414**2) + 0.8974794 / (x2 - 9.896161**2)
     )
 
-    return np.sqrt(1 + six_term)
+    return np.sqrt(1 + three_term)