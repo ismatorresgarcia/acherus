401c8741c023ee21a46b586717d7442b9c784db1
Ismael <126611721+glabu@users.noreply.github.com>
2025-11-13 13:58:07 +0100

    Optimized Raman molecular scattering computation using proper numba parallelization.

--- acherus/functions/raman.py
+++ acherus/functions/raman.py
@@ -1,32 +1,32 @@
-"""Molecular Raman scattering (SRS) module."""
+"""Helper module for stimulated Raman scattering (SRS) contribution."""
 
 import numpy as np
 from numba import njit, prange
 
 
 @njit(parallel=True)
-def compute_raman(int_a, ram_a, ram_x_a, t_a, ram_c1_a, ram_c2_a):
+def compute_raman(inten_a, ram_a, ram_x_a, ram_c1_a, ram_c2_a):
     """
-    Compute molecular Raman scattering delayed response for all time steps
-    using the trapezoidal rule. 
+    Compute Raman contribution delayed response for all time steps
+    using the trapezoidal rule.
 
     Parameters
     ----------
-    int_a : function
-        Intensity function at current propagation step.
+    inten_a : (M, N) array_like
+        Intensity at current propagation step.
     ram_a : (M, N) array_like
         Raman response at all time slices.
     ram_x_a : (M, N) array_like
         Complex Raman response at all time slices.
-    t_a : (N,) array_like
-        Time coordinates grid.
     ram_c1_a : float
         Raman frequency coefficient for the first term.
     ram_c2_a : float
         Raman frequency coefficient for the second term.
 
     """
-    for kk in prange(len(t_a) - 1):
-        ram_x_a[:, kk + 1] = ram_c1_a * ram_x_a[:, kk] + ram_c2_a * (int_a[:, kk + 1] + ram_c1_a * int_a[:, kk])
+    nr, nt = inten_a.shape()
+    for ii in prange(nr):
+        for kk in range(nt - 1):
+            ram_x_a[ii, kk + 1] = ram_c1_a * ram_x_a[ii, kk] + ram_c2_a * (inten_a[ii, kk + 1] + ram_c1_a * inten_a[ii, kk])
 
     ram_a[:] = np.imag(ram_x_a)
