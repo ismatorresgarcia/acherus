c3b79a335ca1de96a0706c67960295dee226f150
Ismael <126611721+glabu@users.noreply.github.com>
2026-02-20 15:35:53 +0100

    Added pyFFTW to the requirements. This avoids the usage of SciPy's default as a fallback.

--- acherus/physics/medium.py
+++ acherus/physics/medium.py
@@ -24,7 +24,7 @@ medium properties as direct attributes of the class.
 
 """
 
-from ..functions.dispersion_models import (
+from ..functions.sellmeier import (
     sellmeier_air,
     sellmeier_silica,
     sellmeier_water,
