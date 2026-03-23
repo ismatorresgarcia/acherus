734b7d7585a122e31300a4925223bcea2f5d815a
Ismael <126611721+glabu@users.noreply.github.com>
2026-03-23 11:07:27 +0100

    version: Released version 0.8.1 with new documentation and Read The Docs.

--- release.sh
+++ release.sh
@@ -6,10 +6,12 @@
 set NAME "acherus"
 set VER (python -c "import acherus._version as v; print(v.__version__)")
 
+# Print package name and version
 echo "========================================================================"
 echo "Tagging $NAME v$VER"
 echo "========================================================================"
 
+# Tag and push the new release
 git tag -a v$VER -m "Release $NAME v$VER"
 git push origin v$VER
 echo "✅ acherus v$VER tagged and pushed."
